86cb92ae5892a8e9f8b600246cab07c913f0a63f
Rafael Dias <rafael78@gmail.com>
2019-03-11 19:45:13 -0300

    forgot

---
+++ forgot/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Shopping na sua Casa - Esqueci minha senha</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../vendor/simple-line-icons/css/simple-line-icons.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+              <p><img src="../img/nasuacasa.png" class="img-fluid" alt=""></p>
+            <h5 class="card-title text-center">Crie uma nova senha:</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+                <input type="password" id="passwd1" class="form-control" placeholder="Coloque uma nova senha" required autofocus>
+                <label for="passwd1">Coloque uma nova senha</label>
+              </div>
+
+              <div class="form-label-group">
+                <input type="password" id="passwd2" class="form-control" placeholder="Redigite a nova senha" required>
+                <label for="passwd2">Redigite a nova senha</label>
+              </div>
+
+              
+              <button class="btn btn-lg btn-ssc btn-block text-uppercase" type="submit">Resetar minha senha</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
